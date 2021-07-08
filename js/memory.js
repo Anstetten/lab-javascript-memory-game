1f4502a1a43bc854606ee556728c3c35967cc9f2
Zsolt HORVATH <h.zsola@gmail.com>
2021-07-08 16:32:40 +0200

    Iteration 2 - MemoryGame class is done

--- js/memory.js
+++ js/memory.js
@@ -8,15 +8,38 @@ class MemoryGame {
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (this.cards===undefined) return undefined;
+    //let shuffledCards = cards;
+    
+
+    for (let m=(this.cards.length-1);m>=0;m--){
+      let randomIndex=Math.floor(Math.random()*(m+1));
+      //We make a copy of the picked card
+      let interimCard= this.cards[randomIndex];
+      //Remove it form the original deck
+      this.cards.splice(randomIndex,1);
+      //Put the chosen element to the end of the deck
+      this.cards.push(interimCard);
+    }
+
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked+=1;
+
+    if(card1===card2){
+     this.pairsGuessed+=1;
+     return true;
+    }
+
+    return false;  
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed===this.cards.length/2) return true;
+
+    return false;
   }
 }
 
